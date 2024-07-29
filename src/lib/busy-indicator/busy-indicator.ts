2d8abf35ec375c731dd871dd6881e7b8cdada70f
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-07-29 10:13:57 -0400

    feat: add busy indicatror

--- src/lib/busy-indicator/busy-indicator.ts
+++ src/lib/busy-indicator/busy-indicator.ts
@@ -9,6 +9,8 @@ import { customElement, property } from 'lit/decorators.js';
 
 import styles from './busy-indicator.scss?inline';
 
+export type BusyIndicatorDirection = 'row' | 'column';
+
 @customElement('forge-busy-indicator')
 export class BusyIndicatorElement extends LitElement {
   static {
@@ -40,17 +42,19 @@ export class BusyIndicatorElement extends LitElement {
   public spinner = true;
 
   @property({ type: Boolean, reflect: true })
+  public progressBarDeterminate = false;
+
+  @property({ type: Boolean, reflect: true })
   public progressBar = false;
 
   @property({ type: Number })
   public progress = 0;
 
-  @property({ type: Number })
+  @property({ type: Number, reflect: true })
   public buffer = 0;
 
-  private _onCancel(): void {
-    this.open = false;
-  }
+  @property({ type: String, reflect: true })
+  public direction: BusyIndicatorDirection = 'column';
 
   public override render(): TemplateResult {
     return html`
@@ -60,37 +64,71 @@ export class BusyIndicatorElement extends LitElement {
         aria-labelledby=${this.titleText ? 'title' : nothing}
         aria-describedby="message">
         <div class="surface" part="surface">
-          ${this.titleText ? html`<h1 id="title" class="title" part="title">${this.titleText}</h1>` : nothing}
+          ${this._titleHtml()}
           <div class="message-container" part="message-container">
-            ${this.spinner
-              ? html`<forge-circular-progress
-                  class="spinner"
-                  part="spinner"
-                  aria-hidden="true"></forge-circular-progress>`
-              : nothing}
-            ${!!this.message?.trim()
-              ? html`<p id="message" class="message" part="message">${this.message}</p>`
-              : nothing}
-            ${this.cancel
-              ? html`
-                  <forge-button class="cancel-button" part="cancel-button" @click=${this._onCancel}>
-                    <slot name="cancel-text">Cancel</slot>
-                  </forge-button>
-                `
-              : nothing}
+            ${this._spinnerHtml()} ${this._messageHtml()} ${this._cancelButtonHtml()}
           </div>
-          ${this.progressBar
-            ? html`
-                <div class="progress-container" part="progress-bar-container">
-                  <forge-linear-progress
-                    part="progress-bar"
-                    aria-hidden="true"
-                    .progress=${this.progress}></forge-linear-progress>
-                </div>
-              `
-            : nothing}
+          ${this._progressBarHtml()}
         </div>
       </forge-dialog>
     `;
+  }
+
+  private _onCancel(): void {
+    this.open = false;
+  }
+
+  private _titleHtml(): TemplateResult | typeof nothing {
+    if (this.titleText) {
+      return html`<h1 id="title" class="title" part="title">${this.titleText}</h1>`;
+    }
+
+    return nothing;
+  }
+
+  private _spinnerHtml(): TemplateResult | typeof nothing {
+    if (this.spinner) {
+      return html`<forge-circular-progress class="spinner" part="spinner" aria-hidden="true">
+      </forge-circular-progress>`;
+    }
+
+    return nothing;
+  }
+
+  private _cancelButtonHtml(): TemplateResult | typeof nothing {
+    if (this.cancel) {
+      return html`
+        <forge-button class="cancel-button" part="cancel-button" @click=${this._onCancel}>
+          <slot name="cancel-text">Cancel</slot>
+        </forge-button>
+      `;
+    }
+
+    return nothing;
+  }
+
+  private _messageHtml(): TemplateResult | any {
+    if (!!this.message?.trim()) {
+      return html`<p id="message" class="message" part="message">${this.message}</p>`;
+    }
+
+    return nothing;
+  }
+
+  private _progressBarHtml(): TemplateResult | typeof nothing {
+    if (this.progressBar) {
+      return html`
+        <div class="progress-container" part="progress-bar-container">
+          <forge-linear-progress
+            part="progress-bar"
+            aria-hidden="true"
+            .buffer="${this.buffer}"
+            .determinate="${this.progressBarDeterminate}"
+            .progress=${this.progress}></forge-linear-progress>
+        </div>
+      `;
+    }
+
+    return nothing;
   }
 }
