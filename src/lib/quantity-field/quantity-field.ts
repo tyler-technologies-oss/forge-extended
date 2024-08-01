d3983c0caaae828334b84463ee10998610e918ac
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-08-01 16:34:06 -0400

    chore: adjust quantity field to provide built-in increment/decrement buttons and text field

--- src/lib/quantity-field/quantity-field.ts
+++ src/lib/quantity-field/quantity-field.ts
@@ -1,11 +1,12 @@
-import { LitElement, PropertyValues, TemplateResult, html, unsafeCSS } from 'lit';
-import { customElement, property, queryAssignedElements } from 'lit/decorators.js';
+import { LitElement, TemplateResult, html, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import {
   defineTextFieldComponent,
   defineIconButtonComponent,
   defineIconComponent,
-  TextFieldComponent
+  IconRegistry
 } from '@tylertech/forge';
+import { tylIconPlus, tylIconMinus } from '@tylertech/tyler-icons/extended';
 
 import styles from './quantity-field.scss?inline';
 
@@ -21,6 +22,8 @@ export class QuantityFieldElement extends LitElement {
     defineTextFieldComponent();
     defineIconButtonComponent();
     defineIconComponent();
+
+    IconRegistry.define([tylIconMinus, tylIconPlus]);
   }
 
   public static override styles = unsafeCSS(styles);
@@ -37,30 +40,40 @@ export class QuantityFieldElement extends LitElement {
   @property({ type: Boolean, reflect: true })
   public required = false;
 
-  @queryAssignedElements({ flatten: true, selector: 'forge-text-field' })
-  private textFields!: TextFieldComponent[];
+  /**
+   * The accessible label for the decrement button.
+   */
+  @property()
+  public decrementLabel = 'Decrement';
+
+  /**
+   * The accessible label for the increment button.
+   */
+  @property()
+  public incrementLabel = 'Increment';
 
   public override render(): TemplateResult {
     return html`
       <div class="container">
         <slot name="label"></slot>
         <div class="inner">
-          <slot name="decrement-button" @click=${this._onDecrement}></slot>
-          <slot></slot>
-          <slot name="increment-button" @click=${this._onIncrement}></slot>
+          <slot name="decrement-button" @click=${this._onDecrement}>
+            <forge-icon-button shape="squared" aria-label=${this.decrementLabel}>
+              <forge-icon name="minus"></forge-icon>
+            </forge-icon-button>
+          </slot>
+          <forge-text-field .invalid=${this.invalid} .required=${this.required}>
+            <slot></slot>
+          </forge-text-field>
+          <slot name="increment-button" @click=${this._onIncrement}>
+            <forge-icon-button shape="squared" aria-label=${this.incrementLabel}>
+              <forge-icon name="plus"></forge-icon>
+            </forge-icon-button>
+          </slot>
         </div>
         <slot name="support-text"></slot>
       </div>
     `;
-  }
-
-  // eslint-disable-next-line @tylertech-eslint/require-private-modifier
-  protected override updated(changedProperties: PropertyValues<this>): void {
-    super.updated(changedProperties);
-
-    if (changedProperties.has('invalid') && this.textFields[0]) {
-      this.textFields[0].invalid = this.invalid;
-    }
   }
 
   private _tryGetInput(): HTMLInputElement {
--- src/stories/components/quantity-field/QuantityField.stories.ts
+++ src/stories/components/quantity-field/QuantityField.stories.ts
@@ -1,13 +1,9 @@
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
-import { IconRegistry } from '@tylertech/forge';
-import { tylIconPlus, tylIconMinus } from '@tylertech/tyler-icons/extended';
 
 import '$lib/quantity-field/quantity-field';
 
 const component = 'forge-quantity-field';
-
-IconRegistry.define([tylIconPlus, tylIconMinus]);
 
 const meta = {
   title: 'Components/Quantity Field',
@@ -16,15 +12,7 @@ const meta = {
     return html`
       <forge-quantity-field .invalid=${args.invalid} .required=${args.required}>
         <label slot="label" for="quantity">Quantity</label>
-        <forge-icon-button shape="squared" aria-label="Decrement" slot="decrement-button">
-          <forge-icon name="minus"></forge-icon>
-        </forge-icon-button>
-        <forge-text-field>
-          <input id="quantity" type="number" value="1" aria-label="Set a quantity" step="2" />
-        </forge-text-field>
-        <forge-icon-button shape="squared" aria-label="Increment" slot="increment-button">
-          <forge-icon name="plus"></forge-icon>
-        </forge-icon-button>
+        <input id="quantity" type="number" value="1" aria-label="Set a quantity" step="2" />
         <span slot="support-text">Enter a quantity</span>
       </forge-quantity-field>
     `;
