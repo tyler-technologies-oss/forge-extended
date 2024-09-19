f18b6380cc8565bfb47ffff2720422a46c336b44
nickonometry <nickonometry@gmail.com>
2024-09-19 13:12:57 -0400

    chore: brought in the forge dialog component baked into the confirmation

--- src/lib/confirmation-dialog/confirmation-dialog.ts
+++ src/lib/confirmation-dialog/confirmation-dialog.ts
@@ -1,5 +1,5 @@
-import { defineButtonComponent } from '@tylertech/forge';
-import { customElement, property } from 'lit/decorators.js';
+import { defineButtonComponent, defineDialogComponent } from '@tylertech/forge';
+import { customElement, property, state } from 'lit/decorators.js';
 
 import styles from './confirmation-dialog.scss?inline';
 import { html, LitElement, nothing, TemplateResult, unsafeCSS } from 'lit';
@@ -24,15 +24,25 @@ declare global {
 export class ConfirmationDialogElement extends LitElement {
   static {
     defineButtonComponent();
+    defineDialogComponent();
   }
 
   public static override styles = unsafeCSS(styles);
 
   /**
-   * Indicates whether the confirmation dialog is open.
+   * Indicates whether the busy indicator is open.
    */
   @property({ type: Boolean, reflect: true })
   public open = false;
+
+  @property({ type: Boolean, reflect: true })
+  public isBusy = false;
+
+  /**
+   * Indicates whether the title is displayed.
+   */
+  @property({ type: Boolean, reflect: true, attribute: 'show-title' })
+  public showTitleText = true;
 
   /**
    * The title text to display.
@@ -49,58 +59,59 @@ export class ConfirmationDialogElement extends LitElement {
   /**
    * Indicates whether the secondary action button is displayed.
    */
-  @property({ type: Boolean, reflect: true, attribute: 'secondary-action-button' })
-  public showSecondaryActionButton = false;
+  @property({ type: Boolean, reflect: true, attribute: 'show-secondary-button' })
+  public showSecondaryButton = false;
 
   /**
    * The secondary action button text to display.
    */
   @property({ attribute: 'secondary-button-text' })
-  public secondaryActionButtonText?: string;
+  public secondaryButtonText?: string;
 
   /**
-   * The secondary action button text to display.
+   * The primary action button text to display.
    */
   @property({ attribute: 'primary-button-text' })
-  public primaryActionButtonText?: string;
+  public primaryButtonText?: string;
 
   public override render(): TemplateResult {
-    const title = this.titleText ? html`<h1 id="title" class="title" part="title">${this.titleText}</h1>` : nothing;
+    const title = this.showTitleText ? html`<h1 id="title" class="title">${this.titleText}</h1>` : nothing;
 
-    const message = !!this.message?.trim()
-      ? html`<p id="message" class="message" part="message">${this.message}</p>`
+    const message = !!this.message?.trim() ? html`<p id="message" class="message">${this.message}</p>` : nothing;
+
+    const busyIndicator = this.isBusy
+      ? html`<forge-circular-progress slot="end" aria-label="Loading"> </forge-circular-progress>`
       : nothing;
 
-    const secondaryActionButton = this.showSecondaryActionButton
-      ? html`<forge-button
-          class="cancel-button"
-          part="cancel-button"
-          variant="outlined"
-          @click=${() => this._onAction(false)}>
-          <slot name="cancel-text">${this.secondaryActionButtonText}</slot>
+    const secondaryButton = this.showSecondaryButton
+      ? html`<forge-button ?disabled=${this.isBusy} variant="outlined" @click=${() => this._onAction(false)}>
+          <slot name="cancel-text">${this.secondaryButtonText}</slot>
         </forge-button>`
       : nothing;
 
-    const primaryActionButton = this.primaryActionButtonText
-      ? html`<forge-button
-          class="primary-button"
-          part="primary-button"
-          variant="raised"
-          @click=${() => this._onAction(true)}>
-          <slot name="primary-text">${this.primaryActionButtonText}</slot>
+    const primaryButton = this.primaryButtonText
+      ? html`<forge-button ?disabled=${this.isBusy} variant="raised" @click=${() => this._onAction(true)}>
+          <slot name="primary-text">${this.primaryButtonText}</slot>
+          ${busyIndicator}
         </forge-button>`
       : nothing;
 
     return html`
-        <div class="container" part="container">
-          ${title}
-          ${message}
-          <div class="actions-container" part-"actions-container">
-            ${secondaryActionButton}
-            ${primaryActionButton}
-          </div>
+      <forge-dialog
+        ?open=${this.open}
+        @forge-dialog-close=${() => this._resetState()}
+        aria-labelledby=${this.titleText ? 'title' : nothing}
+        aria-describedby="message">
+        <div class="container">
+          ${title} ${message}
+          <div class="actions-container">${secondaryButton} ${primaryButton}</div>
         </div>
+      </forge-dialog>
     `;
+  }
+
+  private _resetState(): void {
+    this.isBusy = false;
   }
 
   private _onAction(isPrimary: boolean): void {
--- src/stories/components/confirmation-dialog/ConfirmationDialog.stories.ts
+++ src/stories/components/confirmation-dialog/ConfirmationDialog.stories.ts
@@ -4,52 +4,59 @@ import { createRef, ref } from 'lit/directives/ref.js';
 
 import '$lib/confirmation-dialog/confirmation-dialog';
 import { ConfirmationDialogElement } from '$lib';
-import { DialogComponent, ToastComponent } from '@tylertech/forge';
 
 const component = 'forge-confirmation-dialog';
 
 const meta = {
   title: 'Components/Confirmation Dialog',
   component,
+
   render: args => {
     const confirmationDialogRef = createRef<ConfirmationDialogElement>();
-    const dialogRef = createRef<DialogComponent>();
 
     function handleClick() {
-      dialogRef.value!.open = true;
-    }
-
-    function showToast(isPrimary: boolean) {
-      isPrimary
-        ? ToastComponent.present({ message: 'Primary action clicked', theme: 'primary' })
-        : ToastComponent.present({ message: 'Secondary action clicked', theme: 'secondary' });
+      confirmationDialogRef.value!.open = true;
     }
 
     function onConfirmationDialogAction(e: IConfirmationDialogAction) {
-      showToast(e.detail.primaryAction);
+      const isPrimary = e.detail.primaryAction;
+      if (!isPrimary) {
+        confirmationDialogRef.value!.open = false;
+        return;
+      } else {
+        confirmationDialogRef.value!.isBusy = true;
+        setTimeout(() => {
+          confirmationDialogRef.value!.open = false;
+        }, args.simulateLoading);
+      }
     }
 
     return html`
       <forge-button variant="raised" @click=${handleClick}>Show Confirmation Dialog</forge-button>
-      <forge-dialog ${ref(dialogRef)}>
-        <forge-confirmation-dialog
-          ${ref(confirmationDialogRef)}
-          @forge-confirmation-dialog-action=${(e: IConfirmationDialogAction) => onConfirmationDialogAction(e)}
-          .titleText="${args.titleText}"
-          .message=${args.message}
-          .showSecondaryActionButton=${args.showSecondaryActionButton}
-          .secondaryActionButtonText=${args.secondaryActionButtonText}
-          .primaryActionButtonText=${args.primaryActionButtonText}>
-        </forge-confirmation-dialog>
-      </forge-dialog>
+      <forge-confirmation-dialog
+        ${ref(confirmationDialogRef)}
+        @forge-confirmation-dialog-action=${(e: IConfirmationDialogAction) => onConfirmationDialogAction(e)}
+        ?open=${args.open}
+        .isBusy=${args.isBusy}
+        .showTitleText=${args.showTitleText}
+        .titleText="${args.titleText}"
+        .message=${args.message}
+        .showSecondaryButton=${args.showSecondaryButton}
+        .secondaryButtonText=${args.secondaryButtonText}
+        .primaryButtonText=${args.primaryButtonText}>
+      </forge-confirmation-dialog>
     `;
   },
   args: {
-    titleText: 'This is the title',
-    message: 'Are you sure you want to permanently delete this file?',
-    showSecondaryActionButton: true,
-    secondaryActionButtonText: 'No',
-    primaryActionButtonText: 'Yes'
+    open: false,
+    isBusy: false,
+    showTitleText: true,
+    titleText: 'Reset settings?',
+    message: 'Are you sure you want to reset your device to its default settings?',
+    showSecondaryButton: true,
+    secondaryButtonText: 'No',
+    primaryButtonText: 'Yes',
+    simulateLoading: '500'
   }
 } satisfies Meta;
 
@@ -57,8 +64,4 @@ export default meta;
 
 type Story = StoryObj;
 
-export const Demo: Story = {
-  args: {
-    cancelable: true
-  }
-};
+export const Demo: Story = {};
