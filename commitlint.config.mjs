2929ebcb5228e5a71c485f72abaaa69bfd08248a
nickonometry <nickonometry@gmail.com>
2024-10-03 08:20:49 -0400

    chore: attempting to fix linting issues

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,8 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [0, 'always', 100],
-    'footer-max-line-length': [0, 'always', 100],
-    'lit-plugin.rules.no-complex-attribute-binding': 'off',
-    'lit-plugin.rules.no-incompatible-type-binding': 'off'
+    'footer-max-line-length': [0, 'always', 100]
   }
 };
