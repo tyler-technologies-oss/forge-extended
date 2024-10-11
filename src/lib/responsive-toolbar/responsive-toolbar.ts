c54c7ffb61d9431f0d7e6b3e1855fea972e7ffe4
nickonometry <nickonometry@gmail.com>
2024-10-11 12:45:44 -0400

    chore: code cleanup

--- src/lib/responsive-toolbar/responsive-toolbar.ts
+++ src/lib/responsive-toolbar/responsive-toolbar.ts
@@ -1,18 +1,11 @@
-import { LitElement, PropertyValues, TemplateResult, html, nothing, unsafeCSS } from 'lit';
+import { LitElement, TemplateResult, html, nothing, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import {
-  defineIconButtonComponent,
-  defineIconComponent,
-  IconRegistry,
-  defineToolbarComponent,
-  defineButtonComponent
-} from '@tylertech/forge';
+import { defineToolbarComponent, defineButtonComponent } from '@tylertech/forge';
 import { tylIconPlus, tylIconMinus } from '@tylertech/tyler-icons/extended';
 import { ref, createRef } from 'lit/directives/ref.js';
 import { ResizeController } from '@lit-labs/observers/resize-controller.js';
 import { state } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
-
 import styles from './responsive-toolbar.scss?inline';
 
 declare global {
@@ -29,28 +22,40 @@ export class ResponsiveToolbar extends LitElement {
     super();
   }
 
+  /**
+   * Internal state to determine if the toolbar title is overlapping the actions
+   */
   @state()
   private _isOverflowing = false;
 
+  /**
+   * Maps to the internal toolbar auto-height attribute
+   */
   @property({ type: Boolean, reflect: true, attribute: 'auto-height' })
   public autoHeight = true;
 
+  /**
+   * Maps to the internal toolbar no-divider attribute
+   */
   @property({ type: Boolean, reflect: true, attribute: 'no-divider' })
   public noDivider = false;
 
   static {
     defineToolbarComponent();
-    defineIconButtonComponent();
-    defineIconComponent();
     defineButtonComponent();
-
-    IconRegistry.define([tylIconMinus, tylIconPlus]);
   }
 
   public static override styles = unsafeCSS(styles);
 
+  /**
+   * Element refs that are used to calculate the overflow of the title and actions
+   */
   public titleContainer = createRef<HTMLElement>();
   public buttonContainer = createRef<HTMLElement>();
+
+  /**
+   * Resize observer controller to detect changes in the toolbar width
+   */
   private _resizeController = new ResizeController(this, {});
 
   public override render(): TemplateResult {
