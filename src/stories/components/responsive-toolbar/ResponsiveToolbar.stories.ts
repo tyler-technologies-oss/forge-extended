a0d13f542d3e5a0162073e7bf7d25bdae5a63cc3
nickonometry <nickonometry@gmail.com>
2024-11-13 09:29:41 -0500

    chore: added a range knob to the storybook demo

--- src/stories/components/responsive-toolbar/ResponsiveToolbar.stories.ts
+++ src/stories/components/responsive-toolbar/ResponsiveToolbar.stories.ts
@@ -22,46 +22,50 @@ const meta = {
     ];
 
     return html`
-      <forge-card style="--forge-card-padding: 0;">
-        <forge-responsive-toolbar>
-          <forge-icon-button aria-label="Icon button demo" slot="before-start">
-            <forge-icon name="arrow_back" external></forge-icon>
-          </forge-icon-button>
-          <div slot="start" class="forge-typography--heading4">This is a really really long title</div>
-          <forge-stack inline alignment="center" slot="actions-desktop">
-            ${options.map(
-              item => html` <forge-button variant=${ifDefined(item.variant)}>${item.label}</forge-button> `
-            )}
-          </forge-stack>
-          <div slot="actions-mobile">
-            <forge-menu .options=${options}>
-              <forge-icon-button aria-label="Icon button demo">
-                <forge-icon name="more_vert" external></forge-icon>
-              </forge-icon-button>
-            </forge-menu>
-          </div>
-        </forge-responsive-toolbar>
+      <div style="width: ${args.demoWidth}px">
+        <forge-card style="--forge-card-padding: 0;">
+          <forge-responsive-toolbar>
+            <forge-icon-button aria-label="Icon button demo" slot="before-start">
+              <forge-icon name="arrow_back" external></forge-icon>
+            </forge-icon-button>
+            <div slot="start" class="forge-typography--heading4">This is a really really long title</div>
+            <forge-stack inline alignment="center" slot="actions-desktop">
+              ${options.map(
+                item => html` <forge-button variant=${ifDefined(item.variant)}>${item.label}</forge-button> `
+              )}
+            </forge-stack>
+            <div slot="actions-mobile">
+              <forge-menu .options=${options}>
+                <forge-icon-button aria-label="Icon button demo">
+                  <forge-icon name="more_vert" external></forge-icon>
+                </forge-icon-button>
+              </forge-menu>
+            </div>
+          </forge-responsive-toolbar>
 
-        <div style="padding: 16px;">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </div>
-      </forge-card>
+          <div style="padding: 16px;">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </div>
+        </forge-card>
+      </div>
     `;
   },
   argTypes: {
     invalid: { control: 'boolean' },
-    required: { control: 'boolean' }
+    required: { control: 'boolean' },
+    demoWidth: { control: { type: 'range', min: 360, max: 1260, step: 3 } }
   },
   args: {
     invalid: false,
-    required: false
+    required: false,
+    demoWidth: 900
   }
 } satisfies Meta;
 
