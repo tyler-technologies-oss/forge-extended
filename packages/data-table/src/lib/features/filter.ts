318383974719210211b86a731e55a37ee3d1656c
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-01-13 10:37:08 -0500

    chore: remove unreleased data table component

--- packages/data-table/src/lib/features/filter.ts
+++
@@ -1,6 +0,0 @@
-import { defineSelectComponent, defineTextFieldComponent } from '@tylertech/forge';
-
-export function filterModule(): void {
-  defineSelectComponent();
-  defineTextFieldComponent();
-}
