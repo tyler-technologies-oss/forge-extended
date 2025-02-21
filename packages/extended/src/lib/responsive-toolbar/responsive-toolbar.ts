c0118a148cd525dad7b1703ecbf085ef87650a60
nickonometry <nickonometry@gmail.com>
2025-02-21 09:47:03 -0500

    chore: migrated toolbar code over from the old branch

--- packages/extended/src/lib/responsive-toolbar/responsive-toolbar.ts
+++ packages/extended/src/lib/responsive-toolbar/responsive-toolbar.ts
@@ -1,13 +1,19 @@
-import { LitElement, TemplateResult, html, unsafeCSS } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { LitElement, TemplateResult, html, nothing, unsafeCSS } from 'lit';
+import { ref, createRef } from 'lit/directives/ref.js';
+import { ResizeController } from '@lit-labs/observers/resize-controller.js';
+import { customElement, property, state } from 'lit/decorators.js';
+import { styleMap } from 'lit/directives/style-map.js';
 
 import styles from './responsive-toolbar.scss?inline';
+import { defineToolbarComponent } from '@tylertech/forge';
 
 declare global {
   interface HTMLElementTagNameMap {
     'forge-responsive-toolbar': ResponsiveToolbarComponent;
   }
 }
+
+const BUFFER = 24;
 
 export const ResponsiveToolbarComponentTagName: keyof HTMLElementTagNameMap = 'forge-responsive-toolbar';
 
@@ -16,15 +22,75 @@ export const ResponsiveToolbarComponentTagName: keyof HTMLElementTagNameMap = 'f
  */
 @customElement(ResponsiveToolbarComponentTagName)
 export class ResponsiveToolbarComponent extends LitElement {
+  constructor() {
+    super();
+  }
+
+  static {
+    defineToolbarComponent();
+  }
+
   public static override styles = unsafeCSS(styles);
 
   /**
-   * Description of the property.
+   * Internal state to determine if the toolbar title is overlapping the actions
    */
-  @property({ type: Boolean, attribute: 'some-property' })
-  public someProperty = true;
+  @state()
+  private _isOverflowing = false;
+
+  /**
+   * Maps to the internal toolbar auto-height attribute
+   */
+  @property({ type: Boolean, reflect: true, attribute: 'auto-height' })
+  public autoHeight = true;
+
+  /**
+   * Maps to the internal toolbar no-divider attribute
+   */
+  @property({ type: Boolean, reflect: true, attribute: 'no-divider' })
+  public noDivider = false;
+
+  /**
+   * Element refs that are used to calculate the overflow of the title and actions
+   */
+  public titleContainer = createRef<HTMLElement>();
+  public buttonContainer = createRef<HTMLElement>();
+
+  /**
+   * Resize observer controller to detect changes in the toolbar width
+   */
+  private _resizeController = new ResizeController(this, {});
 
   public override render(): TemplateResult {
-    return html`<div>ResponsiveToolbarComponent works!</div>`;
+    const visibilityStyles = { visibility: 'hidden', position: 'absolute', right: 8 };
+    const titleInlineEndEdge = this.titleContainer.value?.getBoundingClientRect().right || 0;
+    const actionsInlineStartEdge = this.buttonContainer.value?.getBoundingClientRect().left || 0;
+
+    if (titleInlineEndEdge + BUFFER >= actionsInlineStartEdge) {
+      this._isOverflowing = true;
+    } else {
+      this._isOverflowing = false;
+    }
+
+    return html`
+      <forge-toolbar ?auto-height=${this.autoHeight} ?no-divider=${this.noDivider}>
+        <slot name="before-start" slot="before-start"></slot>
+        <div ${ref(this.titleContainer)} slot="start">
+          <slot name="start"></slot>
+        </div>
+
+        <div
+          slot="end"
+          ${ref(this.buttonContainer)}
+          style=${this._isOverflowing ? styleMap(visibilityStyles) : nothing}>
+          <slot name="actions-desktop"></slot>
+        </div>
+
+        <div slot="end" style=${!this._isOverflowing ? styleMap(visibilityStyles) : nothing}>
+          <slot name="actions-mobile"></slot>
+        </div>
+        <slot name="after-end" slot="after-end"></slot>
+      </forge-toolbar>
+    `;
   }
 }
--- packages/extended/src/stories/components/responsive-toolbar/ResponsiveToolbar.stories.ts
+++ packages/extended/src/stories/components/responsive-toolbar/ResponsiveToolbar.stories.ts
@@ -1,18 +1,66 @@
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 import '$lib/responsive-toolbar';
 
 const component = 'forge-responsive-toolbar';
+import { IMenuOption } from '@tylertech/forge';
+
+interface customMenuOption extends IMenuOption {
+  variant?: 'text' | 'raised' | 'outlined';
+}
 
 const meta = {
   title: 'Components/Responsive Toolbar',
   render: args => {
-    return html`<forge-responsive-toolbar></forge-responsive-toolbar>`;
+    const options: customMenuOption[] = [
+      { label: 'Add User ', value: 'add-user', variant: 'text' },
+      { label: 'Remove User', value: 'remove-user', variant: 'outlined' },
+      { label: 'Third action', value: 'third-action', variant: 'raised' }
+    ];
+
+    return html`<div style="width: ${args.demoWidth}px">
+      <forge-card style="--forge-card-padding: 0;">
+        <forge-responsive-toolbar>
+          <forge-icon-button aria-label="Icon button demo" slot="before-start">
+            <forge-icon name="arrow_back" external></forge-icon>
+          </forge-icon-button>
+          <div slot="start" class="forge-typography--heading4">This is a really really long title</div>
+          <forge-stack inline alignment="center" slot="actions-desktop">
+            ${options.map(
+              item => html` <forge-button variant=${ifDefined(item.variant)}>${item.label}</forge-button> `
+            )}
+          </forge-stack>
+          <div slot="actions-mobile">
+            <forge-menu .options=${options}>
+              <forge-icon-button aria-label="Icon button demo">
+                <forge-icon name="more_vert" external></forge-icon>
+              </forge-icon-button>
+            </forge-menu>
+          </div>
+        </forge-responsive-toolbar>
+
+        <div style="padding: 16px;">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </forge-card>
+    </div>`;
   },
   component,
-  argTypes: {},
-  args: {}
+  argTypes: {
+    demoWidth: { control: { type: 'range', min: 360, max: 1260, step: 3 } }
+  },
+  args: {
+    demoWidth: 900
+  }
 } satisfies Meta;
 
 export default meta;
