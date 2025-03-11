5f4b76a6a5842c0ffac01a196c6ea55cb15a282c
nickonometry <nickonometry@gmail.com>
2025-03-11 10:19:10 -0400

    chore(responsive-toolbar): fixed an issue with duplicate imports

--- packages/extended/src/lib/responsive-toolbar/responsive-toolbar.ts
+++ packages/extended/src/lib/responsive-toolbar/responsive-toolbar.ts
@@ -1,33 +1,3 @@
-import { LitElement, TemplateResult, html, unsafeCSS } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-
-import styles from './responsive-toolbar.scss?inline';
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'forge-responsive-toolbar': ResponsiveToolbarComponent;
-  }
-}
-
-export const ResponsiveToolbarComponentTagName: keyof HTMLElementTagNameMap = 'forge-responsive-toolbar';
-
-/**
- * @tag forge-responsive-toolbar
- */
-@customElement(ResponsiveToolbarComponentTagName)
-export class ResponsiveToolbarComponent extends LitElement {
-  public static override styles = unsafeCSS(styles);
-
-  /**
-   * Description of the property.
-   */
-  @property({ type: Boolean, attribute: 'some-property' })
-  public someProperty = true;
-
-  public override render(): TemplateResult {
-    return html`<div>ResponsiveToolbarComponent works!</div>`;
-  }
-}
 import { ResizeController } from '@lit-labs/observers/resize-controller.js';
 import { defineToolbarComponent } from '@tylertech/forge';
 import { LitElement, TemplateResult, html, unsafeCSS, type CSSResult } from 'lit';
