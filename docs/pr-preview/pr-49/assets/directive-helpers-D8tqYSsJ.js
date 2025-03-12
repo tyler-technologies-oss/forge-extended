10eeb523c548605a6730eaa3d1a9cf16e01ebcad
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-03-12 15:14:31 +0000

    Remove preview for PR 49 🛬

--- docs/pr-preview/pr-49/assets/directive-helpers-D8tqYSsJ.js
+++
@@ -1,5 +0,0 @@
-import"./lit-element-CxMangmT.js";/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const d=(e,n)=>n===void 0?(e==null?void 0:e._$litType$)!==void 0:(e==null?void 0:e._$litType$)===n,f=e=>e.strings===void 0;export{d as e,f};
