5393f6f4b0c6044a2d070a0f4dcf78c016c5f5fd
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-03-12 15:08:51 +0000

    Deploy preview for PR 49 🛫

---
+++ docs/pr-preview/pr-49/assets/Home-BJn_gvhB.js
@@ -0,0 +1,9 @@
+import{j as e,M as s}from"./index-C5DREM_I.js";import{useMDXComponents as n}from"./index-D1h2IDb8.js";import"./iframe-D6P7I7lz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";function r(o){const t={a:"a",h1:"h1",img:"img",p:"p",...n(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Tyler Forge™ Extended"}),`
+`,e.jsxs(t.h1,{id:"tyler-forge-extended-",children:["Tyler Forge™ Extended ",e.jsx(t.a,{href:"https://github.com/tyler-technologies-oss/forge-extended/",rel:"nofollow",children:e.jsx(t.img,{src:"https://img.shields.io/github/stars/tyler-technologies-oss/forge-extended",alt:"GitHub Repo stars"})})]}),`
+`,e.jsxs(t.p,{children:[`The Tyler Forge™ Extended library is a set of high quality, accessible UI components that are built on top of the primitive
+`,e.jsx(t.a,{href:"https://forge.tylerdev.io/",rel:"nofollow",children:"Tyler Forge™ components"}),`. These components are typically larger, more complex, and encapsulate
+a larger pattern or design to help developers create user interfaces more efficiently. The main focus of these components is
+strict design and accessibility, and they are intended to offer limited volatility. The components in this library are
+intended to be used in conjunction with the core Tyler Forge library.`]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.a,{href:"https://www.npmjs.com/package/@tylertech/forge-extended",rel:"nofollow",children:e.jsx(t.img,{src:"https://img.shields.io/npm/v/%40tylertech%2Fforge-extended?style=for-the-badge",alt:"NPM Version"})}),`
+`,e.jsx(t.a,{href:"https://www.npmjs.com/package/@tylertech/forge-extended",rel:"nofollow",children:e.jsx(t.img,{src:"https://img.shields.io/npm/dt/%40tylertech%2Fforge-extended?style=for-the-badge",alt:"NPM Downloads"})})]})]})}function m(o={}){const{wrapper:t}={...n(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{m as default};
