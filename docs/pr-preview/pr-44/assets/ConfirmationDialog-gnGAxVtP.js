03abca5c79f8ee78dd04b7c27539aa269df805e4
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-03-12 15:25:57 +0000

    Deploy preview for PR 44 🛫

---
+++ docs/pr-preview/pr-44/assets/ConfirmationDialog-gnGAxVtP.js
@@ -0,0 +1,16 @@
+import{j as i,M as s,T as r,C as a}from"./index-BfYzUxWy.js";import{useMDXComponents as e}from"./index-CjIZq92V.js";import{C as c}from"./CustomArgTypes-Yv_qvX5m.js";import{C as l,D as m}from"./ConfirmationDialog.stories-B0KYlego.js";import"./iframe-CTHNvKra.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-g_Fer-QB.js";import"./lit-element-CxMangmT.js";import"./base-CWxi9ukh.js";import"./when-CamHaXlU.js";import"./directive-helpers-D8tqYSsJ.js";function t(o){const n={blockquote:"blockquote",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...e(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(s,{of:l}),`
+`,i.jsx(r,{}),`
+`,i.jsx(n.p,{children:"A confirmation dialog is a modal component used to present users with a brief message or alert requiring their decision. It typically includes two actions:"}),`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:'Primary action – The main action that confirms the decision (e.g., "Delete", "Submit", "Confirm").'}),`
+`,i.jsx(n.li,{children:'Secondary action – An alternative or dismissive action (e.g., "Cancel", "Go back"), allowing users to exit without proceeding.'}),`
+`]}),`
+`,i.jsx(n.p,{children:"For succinct alerts (one sentence or less), the dialog title is usually omitted to maintain a clean and focused UI. However, for longer messages or critical actions, a title can provide additional clarity."}),`
+`,i.jsxs(n.blockquote,{children:[`
+`,i.jsxs(n.p,{children:[i.jsx(n.strong,{children:"Note:"})," Confirmation dialogs display the same in mobile and desktop contexts."]}),`
+`]}),`
+`,i.jsx(a,{of:m}),`
+`,i.jsx(n.h2,{id:"api",children:"API"}),`
+`,i.jsx(c,{}),`
+`,i.jsx(n.h2,{id:"accessibility",children:"Accessibility"}),`
+`,i.jsx(n.p,{children:"In progress"})]})}function M(o={}){const{wrapper:n}={...e(),...o.components};return n?i.jsx(n,{...o,children:i.jsx(t,{...o})}):t(o)}export{M as default};
