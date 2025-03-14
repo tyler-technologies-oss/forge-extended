28d45c20999ba727a9ab70f3bdb39bbac9615d61
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-03-14 13:43:41 +0000

    Deploy preview for PR 44 🛫

--- docs/pr-preview/pr-44/assets/QuantityField-Bir19A9B.js
+++ docs/pr-preview/pr-44/assets/QuantityField-Bir19A9B.js
@@ -1,4 +1,4 @@
-import{j as e,M as r,T as s,C as d}from"./index-BJ19CvTk.js";import{useMDXComponents as o}from"./index-CpOrBDtn.js";import{C as a}from"./CustomArgTypes-Dz_OfU6d.js";import{Q as l,D as c}from"./QuantityField.stories-CasXk9b9.js";import"./iframe-C84Hud8N.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-DSwJL8BC.js";import"./v4-CtRu48qb.js";import"./lit-element-CxMangmT.js";import"./base-Bk1rqh9v.js";function i(n){const t={code:"code",h2:"h2",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+import{j as e,M as r,T as s,C as d}from"./index-BAukoxfM.js";import{useMDXComponents as o}from"./index-B_0JVg-B.js";import{C as a}from"./CustomArgTypes-CGcNBjzH.js";import{Q as l,D as c}from"./QuantityField.stories-I3ler6zC.js";import"./iframe-C74_R9dZ.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-DSwJL8BC.js";import"./index-CtjQfPVU.js";import"./v4-CtRu48qb.js";import"./lit-element-CxMangmT.js";import"./base-Bk1rqh9v.js";function i(n){const t={code:"code",h2:"h2",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
 `,e.jsx(s,{}),`
 `,e.jsx(t.p,{children:"Quantity fields are used to input a numeric value for a field. They are often used in forms to allow users to increment or decrement a value."}),`
 `,e.jsxs(t.p,{children:['The quantity field is a "decorator" style component, meaning it wraps and enhances the provided ',e.jsx(t.code,{children:"<input>"}),` element with buttons to increment and
@@ -10,4 +10,4 @@ otherwise the component handles the styling and orchestration of element as a wh
 input field, while the `,e.jsx(t.code,{children:"required"})," prop will add an asterisk to the label. You may be required to also set the ",e.jsx(t.code,{children:"required"}),` attribute on the input field
 itself depending on your form validation requirements.`]}),`
 `,e.jsx(t.h2,{id:"api",children:"API"}),`
-`,e.jsx(a,{})]})}function b(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{b as default};
+`,e.jsx(a,{})]})}function C(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{C as default};
