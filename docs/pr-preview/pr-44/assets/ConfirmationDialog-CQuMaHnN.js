da01de3bc62bb777602ace8fca069942eac4c3ae
nickonometry <nickonometry@users.noreply.github.com>
2025-03-14 00:43:28 +0000

    Deploy preview for PR 44 🛫

---
+++ docs/pr-preview/pr-44/assets/ConfirmationDialog-CQuMaHnN.js
@@ -0,0 +1,25 @@
+import{j as e,M as s,T as r,C as l}from"./index-BJ19CvTk.js";import{useMDXComponents as n}from"./index-CpOrBDtn.js";import{C as a}from"./CustomArgTypes-Dz_OfU6d.js";import{C as c,D as d}from"./ConfirmationDialog.stories-DY36ZA0e.js";import"./iframe-C84Hud8N.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-DSwJL8BC.js";import"./lit-element-CxMangmT.js";import"./base-Bk1rqh9v.js";import"./slot-utils-D9dibCyK.js";import"./directive-helpers-D8tqYSsJ.js";function t(o){const i={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:c}),`
+`,e.jsx(r,{}),`
+`,e.jsx(i.p,{children:"A confirmation dialog is a modal component used to present users with a brief message or alert requiring their decision. It typically includes two actions:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:'Primary action – The main action that confirms the decision (e.g., "Delete", "Submit", "Confirm").'}),`
+`,e.jsx(i.li,{children:'Secondary action – An alternative or dismissive action (e.g., "Cancel", "Go back"), allowing users to exit without proceeding.'}),`
+`]}),`
+`,e.jsx(i.p,{children:"For succinct alerts (one sentence or less), the dialog title is usually omitted to maintain a clean and focused UI. However, for longer messages or critical actions, a title can provide additional clarity."}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:[e.jsx(i.strong,{children:"Note:"})," Confirmation dialogs display the same in mobile and desktop contexts."]}),`
+`]}),`
+`,e.jsx(l,{of:d}),`
+`,e.jsx(i.h2,{id:"api",children:"API"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["The confirmation dialog component will add the ",e.jsx(i.code,{children:'role="dialog"'})," and ",e.jsx(i.code,{children:'aria-modal="true"'})," attribute for you."]}),`
+`,e.jsxs(i.li,{children:["Be sure to set the ",e.jsx(i.code,{children:"label"})," and ",e.jsx(i.code,{children:"description"})," attributes on the ",e.jsx(i.code,{children:"<forge-confirmation-dialog>"})," element if needed.",`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"This will allow for a screen reader to properly announce the dialog title and description when it opens."}),`
+`,e.jsxs(i.li,{children:["The ",e.jsx(i.code,{children:"aria-labelledby"})," and ",e.jsx(i.code,{children:"aria-describedby"})," attributes will be set automatically for you based on the ",e.jsx(i.code,{children:"label"})," and ",e.jsx(i.code,{children:"description"})," attributes. If no ",e.jsx(i.code,{children:"label"})," or ",e.jsx(i.code,{children:"description"})," is provided, the component will automatically set these to the content of the title slot and message slot."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.li,{children:["The ",e.jsx(i.code,{children:"busyLabel"})," property allows you to customize the aria-label of the ",e.jsx(i.code,{children:"forge-busy-indicator"})," that becomes visible when ",e.jsx(i.code,{children:"isBusy"}),` is set to true. By default this will announce the word "loading" to the screenreader. It's recommended you set this to a more descriptive value based on the context of your application.`]}),`
+`]})]})}function T(o={}){const{wrapper:i}={...n(),...o.components};return i?e.jsx(i,{...o,children:e.jsx(t,{...o})}):t(o)}export{T as default};
