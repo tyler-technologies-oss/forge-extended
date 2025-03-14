7d33b59a93852a562751a0711b515fa447fe61b3
nickonometry <nickonometry@users.noreply.github.com>
2025-03-14 10:57:17 +0000

    Deploy preview for PR 45 🛫

---
+++ docs/pr-preview/pr-45/assets/BusyIndicator-BsHXtdWK.js
@@ -0,0 +1,50 @@
+import{j as e,M as r,T as a,C as n}from"./index-DsAdr8ZY.js";import{useMDXComponents as o}from"./index-C9XGeXE0.js";import{C as l}from"./CustomArgTypes-H-nZZm78.js";import{B as c,D as d,I as h}from"./BusyIndicator.stories-KDjd3vcM.js";import"./iframe-D3OWKbWW.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-8hDXM9Jl.js";import"./lit-element-CxMangmT.js";import"./ref-3566f8QO.js";import"./directive-helpers-D8tqYSsJ.js";import"./property-NpxWfahL.js";import"./base-Cr0YNUn0.js";function s(i){const t={blockquote:"blockquote",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...o(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:c}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.p,{children:`Busy indicators are a specialized dialog that is used to indicate that a process is running and the user should wait. It is typically used when a process is
+expected to take more than a few seconds to complete. The busy indicator is modal and will prevent the user from interacting with a part, or the rest of the
+application until the process is complete.`}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"}),` The busy indicator is not intended to be used for processes that take less than a few seconds to complete. For processes that take less than a few
+seconds, consider using a progress indicator instead.`]}),`
+`,e.jsxs(t.p,{children:["Use the busy indicator sparingly, as it can be disruptive to the user experience. ",e.jsx(t.strong,{children:"Only use the busy indicator when it is absolutely necessary"}),"."]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"mode",children:"Mode"}),`
+`,e.jsxs(t.p,{children:["Busy indicators are available in two modes: ",e.jsx(t.code,{children:"fullscreen"})," (default) and ",e.jsx(t.code,{children:"inline"}),"."]}),`
+`,e.jsx(t.h3,{id:"fullscreen",children:"Fullscreen"}),`
+`,e.jsx(t.p,{children:`The fullscreen mode is the default mode for the busy indicator. It will display a modal dialog that will prevent the user from interacting with the rest of the
+application until the process is complete. The fullscreen mode is best used when the process is expected to take a long time to complete.`}),`
+`,e.jsx(n,{of:d}),`
+`,e.jsx(t.p,{children:"Fullscreen busy indicators are displayed in the top-layer above all other elements, and use fixed positioning to cover the viewport."}),`
+`,e.jsx(t.h3,{id:"inline",children:"Inline"}),`
+`,e.jsx(t.p,{children:`The inline mode will display the busy indicator inline with the rest of the content. The content will still be rendered within a dialog, but it is non-modal.
+The inline mode is best used when the process is expected to take a short amount of time to complete, while still allowing the user to interact with the rest
+of the application.`}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsxs(t.p,{children:["Inline busy indicators are displayed in the normal flow of the document, use absolute positioning, and will ",e.jsx(t.strong,{children:"not"}),` prevent the user from interacting with the element
+obscured by the busy indicator overlay, or other parts of the page. Make sure to disable user interaction with the obscured element(s) if necessary while the busy
+indicator is visible.`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," When using the inline mode, make sure to provide a ",e.jsx(t.code,{children:"position: relative"}),` style to the parent element of the busy indicator to ensure that it is
+positioned correctly and constrained within the parent element bounds.`]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.p,{children:"The busy indicator is available in multiple variants:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"spinner"})," (default) - A simple spinner that indicates that a process is running. This can be accompanied by an optional title and/or message."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"progress"})," - A progress indicator that indicates the progress of a process. This can be accompanied by an optional title and/or message."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.code,{children:"message-only"})," - A message-only variant that displays a title and/or message to the user. This variant does not include a spinner or progress indicator."]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"cancelable",children:"Cancelable"}),`
+`,e.jsxs(t.p,{children:["The busy indicator can be made cancelable by providing a ",e.jsx(t.code,{children:"cancelable"}),` attribute. This will render a cancel button that the user can click to cancel the process
+that is running. When the process is canceled, the busy indicator will dispatch a `,e.jsx(t.code,{children:"forge-busy-indicator-cancel"}),` event that can be used to handle the cancellation
+however you see fit. You are responsible for closing the busy indicator manually after the event is dispatched.`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsx(t.p,{children:`The busy indicator is a dialog that will prevent the user from interacting with the parts of the page while the process is running. It is important to ensure
+that the busy indicator is accessible to all users, including those using assistive technologies.`}),`
+`,e.jsxs(t.p,{children:["To provide an accessible label for the busy indicator, use the ",e.jsx(t.code,{children:"label"}),` attribute. To also provide a description of the process that is running, use
+the `,e.jsx(t.code,{children:"description"})," attribute. The values will be read out by screen readers to provide context to the user."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," By default the accessible lable and description will use the ",e.jsx(t.code,{children:"titleText"})," and ",e.jsx(t.code,{children:"message"})," properties respectively."]}),`
+`]})]})}function C(i={}){const{wrapper:t}={...o(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(s,{...i})}):s(i)}export{C as default};
---
+++ docs/pr-preview/pr-45/assets/ref-3566f8QO.js
@@ -0,0 +1,13 @@
+import{E as c}from"./lit-element-CxMangmT.js";import{f as $}from"./directive-helpers-D8tqYSsJ.js";/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const l={ATTRIBUTE:1,CHILD:2},u=e=>(...t)=>({_$litDirective$:e,values:t});class _{constructor(t){}get _$AU(){return this._$AM._$AU}_$AT(t,s,n){this._$Ct=t,this._$AM=s,this._$Ci=n}_$AS(t,s){return this.update(t,s)}update(t,s){return this.render(...s)}}/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const h=(e,t)=>{var n;const s=e._$AN;if(s===void 0)return!1;for(const i of s)(n=i._$AO)==null||n.call(i,t,!1),h(i,t);return!0},d=e=>{let t,s;do{if((t=e._$AM)===void 0)break;s=t._$AN,s.delete(e),e=t}while((s==null?void 0:s.size)===0)},f=e=>{for(let t;t=e._$AM;e=t){let s=t._$AN;if(s===void 0)t._$AN=s=new Set;else if(s.has(e))break;s.add(e),p(t)}};function a(e){this._$AN!==void 0?(d(this),this._$AM=e,f(this)):this._$AM=e}function A(e,t=!1,s=0){const n=this._$AH,i=this._$AN;if(i!==void 0&&i.size!==0)if(t)if(Array.isArray(n))for(let r=s;r<n.length;r++)h(n[r],!1),d(n[r]);else n!=null&&(h(n,!1),d(n));else h(this,e)}const p=e=>{e.type==l.CHILD&&(e._$AP??(e._$AP=A),e._$AQ??(e._$AQ=a))};class C extends _{constructor(){super(...arguments),this._$AN=void 0}_$AT(t,s,n){super._$AT(t,s,n),f(this),this.isConnected=t._$AU}_$AO(t,s=!0){var n,i;t!==this.isConnected&&(this.isConnected=t,t?(n=this.reconnected)==null||n.call(this):(i=this.disconnected)==null||i.call(this)),s&&(h(this,t),d(this))}setValue(t){if($(this._$Ct))this._$Ct._$AI(t,this);else{const s=[...this._$Ct._$AH];s[this._$Ci]=t,this._$Ct._$AI(s,this,0)}}disconnected(){}reconnected(){}}/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const T=()=>new Y;class Y{}const o=new WeakMap,N=u(class extends C{render(e){return c}update(e,[t]){var n;const s=t!==this.Y;return s&&this.Y!==void 0&&this.rt(void 0),(s||this.lt!==this.ct)&&(this.Y=t,this.ht=(n=e.options)==null?void 0:n.host,this.rt(this.ct=e.element)),c}rt(e){if(this.isConnected||(e=void 0),typeof this.Y=="function"){const t=this.ht??globalThis;let s=o.get(t);s===void 0&&(s=new WeakMap,o.set(t,s)),s.get(this.Y)!==void 0&&this.Y.call(this.ht,void 0),s.set(this.Y,e),e!==void 0&&this.Y.call(this.ht,e)}else this.Y.value=e}get lt(){var e,t;return typeof this.Y=="function"?(e=o.get(this.ht??globalThis))==null?void 0:e.get(this.Y):(t=this.Y)==null?void 0:t.value}disconnected(){this.lt===this.ct&&this.rt(void 0)}reconnected(){this.rt(this.ct)}});export{u as a,T as e,_ as i,N as n,l as t};
