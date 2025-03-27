5cb3dfcecbe90f58da88eeb3a3c173a1cc984c25
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-03-27 17:55:40 +0000

    Deploy preview for PR 59 🛫

---
+++ docs/pr-preview/pr-59/assets/QuantityField-B_DPJmJM.js
@@ -0,0 +1,13 @@
+import{j as e,M as r,T as s,C as d}from"./index-DHtVRcTz.js";import{useMDXComponents as o}from"./index-CrZfytwH.js";import{C as a}from"./CustomArgTypes-DgaSc5lw.js";import{Q as l,D as m}from"./QuantityField.stories-DAURhmeX.js";import"./iframe-B3e34asb.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BCI4JQNS.js";import"./index-ogSvIofg.js";import"./utils-I5sN4-ip.js";import"./property-BRVZ2yJV.js";import"./v4-CtRu48qb.js";import"./service-adapter-BCkNwAK1.js";import"./lit-element-CxMangmT.js";import"./base-CShCMygk.js";import"./index-ZLv2qr-2.js";import"./with-longpress-listener-CrcfEQjb.js";function i(n){const t={code:"code",h2:"h2",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:"Quantity fields are used to input a numeric value for a field. They are often used in forms to allow users to increment or decrement a value."}),`
+`,e.jsxs(t.p,{children:['The quantity field is a "decorator" style component, meaning it wraps and enhances the provided ',e.jsx(t.code,{children:"<input>"}),` element with buttons to increment and
+decrement the value. You should communicate with the `,e.jsx(t.code,{children:"<input>"}),` as you normally would in terms of getting/setting the value or listening to events,
+otherwise the component handles the styling and orchestration of element as a whole.`]}),`
+`,e.jsx(d,{of:m}),`
+`,e.jsx(t.h2,{id:"invalid-and-required-states",children:"Invalid and Required States"}),`
+`,e.jsxs(t.p,{children:["The quantity field can be marked as invalid or required by setting the ",e.jsx(t.code,{children:"invalid"})," or ",e.jsx(t.code,{children:"required"})," props. The ",e.jsx(t.code,{children:"invalid"}),` prop will add a red border to the
+input field, while the `,e.jsx(t.code,{children:"required"})," prop will add an asterisk to the label. You may be required to also set the ",e.jsx(t.code,{children:"required"}),` attribute on the input field
+itself depending on your form validation requirements.`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(a,{})]})}function D(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{D as default};
