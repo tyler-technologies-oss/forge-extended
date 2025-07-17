7653d9d464d1c83e360c9db452671708d4732a2d
nickonometry <nickonometry@users.noreply.github.com>
2025-07-17 13:38:05 +0000

    Deploy preview for PR 79 🛫

---
+++ docs/pr-preview/pr-79/assets/index-BaGfEVa9.js
@@ -0,0 +1,5 @@
+import{d as o}from"./custom-element-DZpNitAW.js";import"./iframe-BucFrUBr.js";import{I as n}from"./property-B6BUk8Ps.js";/**
+ * @license
+ * Copyright Tyler Technologies, Inc. 
+ * License: Apache-2.0
+ */function i(){o(n)}export{i as d};
