f23f4a0258b9a01a1fd60763fdb80ddf456a91b6
nickonometry <nick.andrews@tylertech.com>
2025-09-16 21:10:43 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge-extended@17d6833582f9c62bfca747f94517015e066fb6b1 🚀

---
+++ docs/v1/assets/ProfileLink.stories-J2LYa4le.js
@@ -0,0 +1,14 @@
+import{x as e}from"./iframe-yoMLdg7V.js";import{I as r,e as t,f as n,g as i}from"./custom-element-BpiCv9Yx.js";import"./profile-link-Dc312oZ3.js";import"./list-C-K0Cq67.js";r.define([t,n,i]);const l="forge-profile-link",p={tags:["hidden"],title:"Components/User Profile/Profile Link",render:a=>e`
+      <forge-profile-link slot="link">
+        <forge-icon slot="icon" name="history"></forge-icon>
+        <a href="http://www.google.com" target="_blank">Audit log</a>
+      </forge-profile-link>
+      <forge-profile-link slot="link">
+        <forge-icon slot="icon" name="settings" external></forge-icon>
+        <a href="http://www.google.com" target="_blank">Application settings</a>
+      </forge-profile-link>
+      <forge-profile-link slot="link">
+        <forge-icon slot="icon" name="account"></forge-icon>
+        <a href="http://www.google.com" target="_blank">Profile</a>
+      </forge-profile-link>
+    `,component:l},o={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const m=["Demo"];export{o as Demo,m as __namedExportsOrder,p as default};
---
+++ docs/v1/assets/WithSelectAllButton.stories-CI9IDD8u.js
@@ -0,0 +1,5 @@
+import{x as r}from"./iframe-yoMLdg7V.js";import"./multi-select-header-3rBL-O3g.js";import"./custom-element-BpiCv9Yx.js";import"./property-DY0DZ6UR.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-zOcBLUw3.js";import"./index-CIwcqdia.js";const{action:o}=__STORYBOOK_MODULE_ACTIONS__,l="forge-multi-select-header",u={tags:["hidden"],title:"Components/Multi Select Header/With Select All Button",render:()=>{const t=o("forge-multi-select-header-select-all");return r`
+      <forge-multi-select-header text="3 items selected" @forge-multi-select-header-select-all=${t}>
+        <span slot="select-all-button-text">Select All Items</span>
+      </forge-multi-select-header>
+    `},component:l},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const h=["Demo"];export{e as Demo,h as __namedExportsOrder,u as default};
