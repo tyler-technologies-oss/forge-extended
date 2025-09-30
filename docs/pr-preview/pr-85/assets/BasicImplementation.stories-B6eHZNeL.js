60f759286a7aafa203f0a3cb3d373f2fce69cb52
nickonometry <nickonometry@users.noreply.github.com>
2025-09-30 16:30:32 +0000

    Deploy preview for PR 85 🛫

--- docs/pr-preview/pr-85/assets/BasicImplementation.stories-B6eHZNeL.js
+++
@@ -1,7 +0,0 @@
-import{x as t}from"./iframe-DSnmWg2t.js";import{I as o,t as r}from"./icon-registry-D9tq7wmu.js";import"./multi-select-header-BxkftN_h.js";import{d as m}from"./index-Bt_kbJ6n.js";import{d as n}from"./index-Cyccj0Oh.js";import"./custom-element-BhZVzxrc.js";import"./property-DHCPBGqf.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-DcREotoq.js";import"./with-default-aria-Cw6jhwwv.js";import"./index-BJCPHirw.js";import"./base-button-adapter-C3elGSpV.js";m();n();o.define([r]);const i="forge-multi-select-header",b={tags:["hidden"],title:"Components/Multi Select Header/Basic Implementation",render:()=>t`
-      <forge-multi-select-header text="3 items selected">
-        <forge-icon-button slot="actions" aria-label="Delete selected">
-          <forge-icon name="delete"></forge-icon>
-        </forge-icon-button>
-      </forge-multi-select-header>
-    `,component:i},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const C=["Demo"];export{e as Demo,C as __namedExportsOrder,b as default};
--- docs/pr-preview/pr-85/assets/WithSelectAllButton.stories-COfNKrdo.js
+++
@@ -1,5 +0,0 @@
-import{x as r}from"./iframe-DSnmWg2t.js";import"./multi-select-header-BxkftN_h.js";import"./custom-element-BhZVzxrc.js";import"./property-DHCPBGqf.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-DcREotoq.js";import"./with-default-aria-Cw6jhwwv.js";import"./icon-registry-D9tq7wmu.js";import"./index-BJCPHirw.js";import"./base-button-adapter-C3elGSpV.js";const{action:o}=__STORYBOOK_MODULE_ACTIONS__,l="forge-multi-select-header",f={tags:["hidden"],title:"Components/Multi Select Header/With Select All Button",render:()=>{const t=o("forge-multi-select-header-select-all");return r`
-      <forge-multi-select-header text="3 items selected" @forge-multi-select-header-select-all=${t}>
-        <span slot="select-all-button-text">Select All Items</span>
-      </forge-multi-select-header>
-    `},component:l},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const g=["Demo"];export{e as Demo,g as __namedExportsOrder,f as default};
