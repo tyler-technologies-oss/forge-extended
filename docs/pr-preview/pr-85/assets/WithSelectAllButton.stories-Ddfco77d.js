72622e3cd976ea04849e8f5c2ad5a7e36f1b5338
nickonometry <nickonometry@users.noreply.github.com>
2025-10-01 13:36:55 +0000

    Deploy preview for PR 85 🛫

---
+++ docs/pr-preview/pr-85/assets/WithSelectAllButton.stories-Ddfco77d.js
@@ -0,0 +1,5 @@
+import{x as r}from"./iframe-CNDlWVDT.js";import"./multi-select-header-BQEN_dhr.js";import"./custom-element-BhZVzxrc.js";import"./property-Dn8cfUGN.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-cPIgyLmU.js";import"./utils-Do612cA4.js";import"./with-default-aria-BdkDfC-v.js";import"./icon-registry-CEPSWL0c.js";import"./index-oumoydZV.js";import"./base-button-adapter-DJNx66BF.js";const{action:o}=__STORYBOOK_MODULE_ACTIONS__,l="forge-multi-select-header",g={tags:["hidden"],title:"Components/Multi Select Header/With Select All Button",render:()=>{const t=o("forge-multi-select-header-select-all");return r`
+      <forge-multi-select-header text="3 items selected" @forge-multi-select-header-select-all=${t}>
+        <span slot="select-all-button-text">Select All Items</span>
+      </forge-multi-select-header>
+    `},component:l},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const S=["Demo"];export{e as Demo,S as __namedExportsOrder,g as default};
