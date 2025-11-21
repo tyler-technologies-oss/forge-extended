8a18fe29b1fbd5f2c058b359a167f166c8921c21
Ross-Blakeney <Ross-Blakeney@users.noreply.github.com>
2025-11-21 17:40:09 +0000

    Deploy preview for PR 95 🛫

--- docs/pr-preview/pr-95/assets/BasicImplementation.stories-CpqeSiZg.js
+++
@@ -1,7 +0,0 @@
-import{x as t}from"./iframe-DNXWbHQS.js";import{I as o,t as r}from"./with-default-aria-DaFgehOB.js";import"./multi-select-header-B6Iu1JqG.js";import{d as n}from"./index-BLfxQEG7.js";import{d as m}from"./index-CBcEBjZo.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-kq5a4akY.js";import"./index-yrKgarMF.js";import"./base-button-adapter-CpVFtTSL.js";n();m();o.define([r]);const i="forge-multi-select-header",h={tags:["hidden"],title:"Components/Multi Select Header/Basic Implementation",render:()=>t`
-      <forge-multi-select-header text="3 items selected">
-        <forge-icon-button slot="actions" aria-label="Delete selected">
-          <forge-icon name="delete"></forge-icon>
-        </forge-icon-button>
-      </forge-multi-select-header>
-    `,component:i},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const x=["Demo"];export{e as Demo,x as __namedExportsOrder,h as default};
---
+++ docs/pr-preview/pr-95/assets/index-C3tSyMWO.js
@@ -0,0 +1,9 @@
+import{n as t,h as e}from"./with-default-aria-BGSn6_HX.js";import"./iframe-DVykgemC.js";import{P as n}from"./popover-DDIFCYYR.js";/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */function p(o){return t({...o,state:!0,attribute:!1})}/**
+ * @license
+ * Copyright Tyler Technologies, Inc. 
+ * License: Apache-2.0
+ */function f(){e(n)}export{f as d,p as r};
