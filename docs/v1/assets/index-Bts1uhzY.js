fea014d86e231a114e2e4a1baffa93619f6eb7cf
Ross-Blakeney <43068700+Ross-Blakeney@users.noreply.github.com>
2025-11-24 15:27:48 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge-extended@f8824bde844b2aa22f0d9b20c3911f7bd27fd53d 🚀

--- docs/v1/assets/index-Bts1uhzY.js
+++
@@ -1,5 +0,0 @@
-import{h as o}from"./with-default-aria-BGSn6_HX.js";import"./iframe-DVykgemC.js";import{I as n}from"./base-button-adapter-BkZK3mXI.js";/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function i(){o(n)}export{i as d};
