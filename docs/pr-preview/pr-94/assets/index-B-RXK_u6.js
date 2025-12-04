dbb2145f7ef307c42c46f53e5519fc13059fb48e
nickonometry <nickonometry@users.noreply.github.com>
2025-12-04 18:42:14 +0000

    Remove preview for PR 94 🛬

--- docs/pr-preview/pr-94/assets/index-B-RXK_u6.js
+++
@@ -1,5 +0,0 @@
-import{h as o}from"./with-default-aria-CkbSVPoO.js";import"./iframe-BfDKhbwP.js";import{L as t}from"./list-CuC5zA3d.js";/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function i(){o(t)}export{i as d};
--- docs/pr-preview/pr-94/assets/index-BihplJxQ.js
+++
@@ -1,9 +0,0 @@
-import{n as t,h as e}from"./with-default-aria-CkbSVPoO.js";import"./iframe-BfDKhbwP.js";import{P as n}from"./popover-x2hGm7XV.js";/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function p(o){return t({...o,state:!0,attribute:!1})}/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function f(){e(n)}export{f as d,p as r};
