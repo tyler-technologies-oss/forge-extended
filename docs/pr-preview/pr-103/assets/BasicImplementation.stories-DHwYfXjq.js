ac732391da3b4f8b216c9c7906dd4b60bee3f5e7
nickonometry <nickonometry@users.noreply.github.com>
2026-03-19 15:25:02 +0000

    Deploy preview for PR 103 🛫

---
+++ docs/pr-preview/pr-103/assets/BasicImplementation.stories-DHwYfXjq.js
@@ -0,0 +1,7 @@
+import{x as o}from"./iframe-B58fgqKv.js";import{I as t,k as r}from"./with-default-aria-BGz_Bw5b.js";import"./multi-select-header-C5dfDxko.js";import{d as n}from"./index-b1WdNkWx.js";import{d as m}from"./index-DqlzJkd-.js";import"./when-DfnyT8j1.js";import"./base-CShCMygk.js";import"./index-DgQ-Rquy.js";import"./index-BvyVlESN.js";import"./base-button-adapter-Na5sKSda.js";n();m();t.define([r]);const i="forge-multi-select-header",h={tags:["hidden"],title:"Components/Multi Select Header/Basic Implementation",render:()=>o`
+      <forge-multi-select-header text="3 items selected">
+        <forge-icon-button slot="actions" aria-label="Delete selected">
+          <forge-icon name="delete"></forge-icon>
+        </forge-icon-button>
+      </forge-multi-select-header>
+    `,component:i},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const x=["Demo"];export{e as Demo,x as __namedExportsOrder,h as default};
--- docs/pr-preview/pr-103/assets/index-Btf8Cvj4.js
+++
@@ -1,5 +0,0 @@
-import{p as o}from"./with-default-aria-CtRqHLx0.js";import"./iframe-BZn7XJ1E.js";import{L as t}from"./list-C5mZHpoZ.js";/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function i(){o(t)}export{i as d};
