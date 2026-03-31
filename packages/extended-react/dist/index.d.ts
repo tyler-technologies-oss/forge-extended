9725fc2df966de6ade7c268e7642819a2dceacdf
nickonometry <nickonometry@gmail.com>
2026-03-31 14:00:39 -0400

    feat(structured-card): re-ran build

--- packages/extended-react/dist/index.d.ts
+++ packages/extended-react/dist/index.d.ts
@@ -1,19 +1,12 @@
 export * from "./ForgeAppLauncher.js";
-<<<<<<< HEAD
-=======
 export * from "./ForgeAppLayout.js";
->>>>>>> main
+export * from "./ForgeConfirmationDialog.js";
 export * from "./ForgeBusyIndicator.js";
-export * from "./ForgeConfirmationDialog.js";
 export * from "./ForgeContentScaffold.js";
-export * from "./ForgeMultiSelectHeader.js";
 export * from "./ForgeQuantityField.js";
+export * from "./ForgeMultiSelectHeader.js";
 export * from "./ForgeResponsiveToolbar.js";
-<<<<<<< HEAD
 export * from "./ForgeStructuredCard.js";
-=======
-export * from "./ForgeConfirmationDialog.js";
->>>>>>> main
 export * from "./ForgeThemeToggle.js";
 export * from "./ForgeUserProfile.js";
 export * from "./ForgeAppLauncherLink.js";
