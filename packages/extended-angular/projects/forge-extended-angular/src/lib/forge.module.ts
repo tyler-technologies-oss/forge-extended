a631ebe164acf6975b6698639866d3837cf3256d
nickonometry <nickonometry@gmail.com>
2026-04-06 12:05:49 -0400

    feat(count-card): add Count Card and Structured Card modules, update public API, and clean up custom elements

--- packages/extended-angular/projects/forge-extended-angular/src/lib/forge.module.ts
+++ packages/extended-angular/projects/forge-extended-angular/src/lib/forge.module.ts
@@ -1,11 +1,14 @@
 import { ForgeExtendedAppLauncherLinkModule } from './app-launcher-link/app-launcher-link.module';
 import { ForgeExtendedAppLauncherModule } from './app-launcher/app-launcher.module';
+import { ForgeExtendedAppLayoutModule } from './app-layout';
 import { ForgeExtendedBusyIndicatorModule } from './busy-indicator/busy-indicator.module';
 import { ForgeExtendedConfirmationDialogModule } from './confirmation-dialog/confirmation-dialog.module';
+import { ForgeExtendedCountCardModule } from './count-card';
 import { ForgeExtendedMultiSelectHeaderModule } from './multi-select-header/multi-select-header.module';
 import { ForgeExtendedProfileLinkModule } from './profile-link/profile-link.module';
 import { ForgeExtendedQuantityFieldModule } from './quantity-field/quantity-field.module';
 import { ForgeExtendedResponsiveToolbarModule } from './responsive-toolbar/responsive-toolbar.module';
+import { ForgeExtendedStructuredCardModule } from './structured-card';
 import { ForgeExtendedUserProfileModule } from './user-profile/user-profile.module';
 import { NgModule } from '@angular/core';
 
@@ -19,7 +22,11 @@ import { NgModule } from '@angular/core';
     ForgeExtendedProfileLinkModule,
     ForgeExtendedQuantityFieldModule,
     ForgeExtendedResponsiveToolbarModule,
-    ForgeExtendedUserProfileModule
+    ForgeExtendedUserProfileModule,
+    ForgeExtendedCountCardModule,
+    ForgeExtendedAppLayoutModule,
+    ForgeExtendedStructuredCardModule,
+    ForgeExtendedCountCardModule
   ]
 })
 export class ForgeExtendedModule {}
