94aa4969a63885588c7efe89dbfb87728f6fb337
nickonometry <nickonometry@users.noreply.github.com>
2026-04-22 18:37:57 +0000

    Remove preview for PR 115 🛬

--- docs/pr-preview/pr-115/assets/state-BpobjxgB.js
+++
@@ -1,5 +0,0 @@
-import{B as r}from"./with-default-aria-CNms5Vty.js";/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function a(t){return r({...t,state:!0,attribute:!1})}export{a as r};
--- docs/pr-preview/pr-115/assets/when-3fO0zp9C.js
+++
@@ -1,5 +0,0 @@
-/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function n(r,t,e){return r?t(r):e?.(r)}export{n};
