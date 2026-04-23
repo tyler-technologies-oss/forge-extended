baa657c3ad57830d5eb13ca4dad6054076524e6d
nickonometry <nickonometry@users.noreply.github.com>
2026-04-23 18:27:51 +0000

    Remove preview for PR 117 🛬

--- docs/pr-preview/pr-117/assets/WithSelectAllButton.stories-C9ah5bOi.js
+++
@@ -1,5 +0,0 @@
-import{x as r}from"./iframe-jIu5SHun.js";import"./multi-select-header-WVQC4H4N.js";import"./constants-BCDXA3X6.js";import"./with-default-aria-CAxpHz7d.js";import"./query-assigned-nodes-cwDSNLIu.js";import"./base-CShCMygk.js";import"./when-3fO0zp9C.js";import"./index-DNX8rawF.js";import"./toolbar-BUJqnE4A.js";import"./index-DN7rpd4A.js";import"./base-button-adapter-44w80FD7.js";import"./focus-indicator-xmVPxQxu.js";import"./icon-BwWww8Mn.js";const{action:o}=__STORYBOOK_MODULE_ACTIONS__,l="forge-multi-select-header",S={tags:["hidden"],title:"Components/Multi Select Header/With Select All Button",render:()=>{const t=o("forge-multi-select-header-select-all");return r`
-      <forge-multi-select-header text="3 items selected" @forge-multi-select-header-select-all=${t}>
-        <span slot="select-all-button-text">Select All Items</span>
-      </forge-multi-select-header>
-    `},component:l},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const O=["Demo"];export{e as Demo,O as __namedExportsOrder,S as default};
--- docs/pr-preview/pr-117/assets/index-DfNAuBth.js
+++
@@ -1,5 +0,0 @@
-import{d as o}from"./constants-BCDXA3X6.js";import"./iframe-jIu5SHun.js";import{T as t}from"./tooltip-DduLWRrp.js";/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function i(){o(t)}export{i as d};
