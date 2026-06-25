4ea9684492fc61a3d1104b438121b60ee853de8d
derekmoss <derekmoss@users.noreply.github.com>
2026-06-25 16:29:39 +0000

    Deploy preview for PR 136 🛫

---
+++ docs/pr-preview/pr-136/assets/BasicImplementation.stories-BVtL8GVK.js
@@ -0,0 +1,7 @@
+import{x as o}from"./iframe-C_q70EjJ.js";import{I as t,m as r}from"./with-default-aria-DoUMoyAO.js";import"./multi-select-header-CrYin0Ls.js";import{d as m}from"./index-DeIFq22o.js";import{d as i}from"./index-a4-2iaGm.js";import"./constants-Bz-XVs7i.js";import"./query-assigned-nodes-cwDSNLIu.js";import"./base-CShCMygk.js";import"./when-3fO0zp9C.js";import"./index-fjB_3uN-.js";import"./toolbar-DYp5PUmZ.js";import"./index-DYHwHMNC.js";import"./base-button-adapter-D8bdyaXz.js";import"./focus-indicator-B5nfyM7B.js";import"./icon-v3qIUZe9.js";import"./icon-button-BYpqW0EQ.js";m();i();t.define([r]);const n="forge-multi-select-header",B={tags:["hidden"],title:"Components/Multi Select Header/Basic Implementation",render:()=>o`
+      <forge-multi-select-header text="3 items selected">
+        <forge-icon-button slot="actions" aria-label="Delete selected">
+          <forge-icon name="delete"></forge-icon>
+        </forge-icon-button>
+      </forge-multi-select-header>
+    `,component:n},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const S=["Demo"];export{e as Demo,S as __namedExportsOrder,B as default};
---
+++ docs/pr-preview/pr-136/assets/QuantityField-BUgXjMRd.js
@@ -0,0 +1,13 @@
+import{j as e,M as r,T as s,C as d}from"./blocks-C0P_eN_e.js";import{useMDXComponents as o}from"./index-De36GQbW.js";import{C as a}from"./CustomArgTypes-CW-DL7-k.js";import{Q as l,D as m}from"./QuantityField.stories-Dh5dVjMR.js";import"./iframe-C_q70EjJ.js";import"./index-BFVZ7Xg9.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Ov9xTcai.js";import"./constants-Bz-XVs7i.js";import"./with-default-aria-DoUMoyAO.js";import"./base-CShCMygk.js";import"./index-4_qBsQJ_.js";import"./base-button-adapter-D8bdyaXz.js";import"./focus-indicator-B5nfyM7B.js";import"./field-BPDtcr-Y.js";import"./icon-button-BYpqW0EQ.js";import"./icon-v3qIUZe9.js";import"./tooltip-BAAsjeAC.js";import"./with-longpress-listener-Dx3vJ2No.js";import"./dismissible-stack-RZL21AAV.js";import"./index-DeIFq22o.js";import"./index-a4-2iaGm.js";function i(n){const t={code:"code",h2:"h2",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:"Quantity fields are used to input a numeric value for a field. They are often used in forms to allow users to increment or decrement a value."}),`
+`,e.jsxs(t.p,{children:['The quantity field is a "decorator" style component, meaning it wraps and enhances the provided ',e.jsx(t.code,{children:"<input>"}),` element with buttons to increment and
+decrement the value. You should communicate with the `,e.jsx(t.code,{children:"<input>"}),` as you normally would in terms of getting/setting the value or listening to events,
+otherwise the component handles the styling and orchestration of element as a whole.`]}),`
+`,e.jsx(d,{of:m}),`
+`,e.jsx(t.h2,{id:"invalid-and-required-states",children:"Invalid and Required States"}),`
+`,e.jsxs(t.p,{children:["The quantity field can be marked as invalid or required by setting the ",e.jsx(t.code,{children:"invalid"})," or ",e.jsx(t.code,{children:"required"})," props. The ",e.jsx(t.code,{children:"invalid"}),` prop will add a red border to the
+input field, while the `,e.jsx(t.code,{children:"required"})," prop will add an asterisk to the label. You may be required to also set the ",e.jsx(t.code,{children:"required"}),` attribute on the input field
+itself depending on your form validation requirements.`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(a,{})]})}function I(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{I as default};
