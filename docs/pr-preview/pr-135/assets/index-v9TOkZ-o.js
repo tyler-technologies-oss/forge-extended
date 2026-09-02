c4a31dee93d94e01fdaf5f5c31f34e0f42097400
derekmoss <derekmoss@users.noreply.github.com>
2026-09-02 18:00:58 +0000

    Remove preview for PR 135 🛬

--- docs/pr-preview/pr-135/assets/index-v9TOkZ-o.js
+++
@@ -1,5 +0,0 @@
-import{d as t}from"./constants-CRIYNPs9.js";import"./iframe-BjE-IuEP.js";import{L as o}from"./list-_5p6RBzw.js";/**
- * @license
- * Copyright Tyler Technologies, Inc. 
- * License: Apache-2.0
- */function i(){t(o)}export{i as d};
--- docs/pr-preview/pr-135/assets/query-assigned-nodes-cwDSNLIu.js
+++
@@ -1,5 +0,0 @@
-import{e as s}from"./base-CShCMygk.js";/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function i(t){return(o,r)=>{const{slot:e}=t??{},n="slot"+(e?`[name=${e}]`:":not([name])");return s(o,r,{get(){return this.renderRoot?.querySelector(n)?.assignedNodes(t)??[]}})}}export{i as n};
